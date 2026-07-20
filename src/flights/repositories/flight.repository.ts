361aa98e1a3af7db84aa768363d3382319416edc
Bhavesh Kumar Singh <kishornand.dib@gmail.com>
2026-07-20 13:23:02 +0530

    feat: created FlightRepository to perform read and write logic on Flight table in Database

---
+++ src/flights/repositories/flight.repository.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/infrastructure/database/prisma.service';
+
+@Injectable()
+export class FlightRepository {
+    constructor(private readonly prisma: PrismaService){}
+
+    //create a flight method this is write only
+    async create(data : {
+        flightNumber: string;
+        origin: string;
+        destination: string;
+        departureTime: Date;
+        arrivalTime: Date;
+    }) {
+        return this.prisma.flight.create({
+            data,
+        });
+    }
+
+
+    //method to get all the flights readonly
+    async findAll() {
+        return this.prisma.flight.findMany();
+    }
+
+    //get details about a particular flight through its id ,readonly
+    async findById(id: number){
+        return this.prisma.flight.findUnique({
+            where: {
+                id,
+            }, 
+        });
+    }
+
+}
