688785efb76ebdef93d54c414836a32ef7236517
Bhavesh Kumar Singh <kishornand.dib@gmail.com>
2026-07-20 12:04:49 +0530

    feat: Established Database Module which has PrismaService class which in turn extends prismaclient

--- src/infrastructure/database/database.module.ts
+++ src/infrastructure/database/database.module.ts
@@ -1,6 +1,7 @@
 import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
+//@Gobal tell nestjs this service is available everywhere no need for imports
 @Global()
 @Module({
   providers: [PrismaService],
--- src/infrastructure/database/prisma.service.ts
+++ src/infrastructure/database/prisma.service.ts
@@ -1,11 +1,10 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
+//PrismaService class extends the prismaclient class and sort of warps around it
+//onMdouleInit and onModuleDestroy are the lifecycle hooks/interfaces for the database connection provided by the nestjs
 @Injectable()
-export class PrismaService
-  extends PrismaClient
-  implements OnModuleInit, OnModuleDestroy
-{
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   async onModuleInit() {
     await this.$connect();
   }
@@ -16,6 +15,7 @@ export class PrismaService
 
   async isHealthy(): Promise<boolean> {
     try {
+      //select 1 is the univeral light weight sql query and asks postgres are you alive
       await this.$queryRaw`SELECT 1`;
       return true;
     } catch {
