f49169cf57297568f90885edb96a3a6554f562af
Bhavesh Kumar Singh <kishornand.dib@gmail.com>
2026-07-21 16:13:06 +0530

    feat: add getUserById method and improve error handling in UsersService

--- src/users/service/users.service.ts
+++ src/users/service/users.service.ts
@@ -1,4 +1,4 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { UserRepository } from '../repositories/user.repository';
 import { CreateUserDto } from '../dto/create-user.dto';
 import { UserResponseDto } from '../dto/user-response.dto';
@@ -7,18 +7,37 @@ import { UserResponseDto } from '../dto/user-response.dto';
 export class UsersService { 
     constructor(private readonly userRepository: UserRepository){}
 
+    //method to create a user
     async createUser(createUserDto: CreateUserDto): Promise<UserResponseDto> {
 
         //checking if the user with current email already exists or not
         const existingUser = await this.userRepository.findByEmail(createUserDto.email);
 
         if(existingUser) {
-            throw new ConflictException('Email already exists!!!');
+            throw new ConflictException('Email already exists');
         }
 
         //creating user
         const user = await this.userRepository.create(createUserDto);
         
+        //mapping prisma model to response dto
+        return {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            clvScore: user.clvScore,
+            createdAt: user.createdAt,
+        };
+    }
+
+    //method to find user by id;
+    async getUserById(id: number): Promise<UserResponseDto>  {
+
+        const user = await this.userRepository.findById(id);
+        if(!user){
+            throw new NotFoundException('User not found');
+        }
+
         //mapping prisma model to response dto
         return {
             id: user.id,
