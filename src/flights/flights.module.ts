f58ffb7b70f89d470139053ef37b54919bbbac9a
Bhavesh Kumar Singh <kishornand.dib@gmail.com>
2026-07-22 16:40:48 +0530

    feat: added SeatsService and include it in FlightsModule providers

--- src/flights/flights.module.ts
+++ src/flights/flights.module.ts
@@ -3,11 +3,13 @@ import { FlightRepository } from './repositories/flight.repository';
 import { SeatRepository } from './repositories/seats.repository';
 import { FlightsService } from './service/flights.service';
 import { FlightsController } from './controller/flights.controller';
+import { SeatsService } from './service/seats.service';
 
 @Module({
     controllers: [FlightsController],
     providers: [
         FlightsService,
+        SeatsService,
         FlightRepository,
         SeatRepository,
     ],
