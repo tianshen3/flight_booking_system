e37ff137388e77592b80008541d8125fa8b4bd92
Bhavesh Kumar Singh <kishornand.dib@gmail.com>
2026-07-22 16:17:52 +0530

    feat: implemented other endpoints like createFlight, getAllFlights, and getFlightById methods

--- src/flights/controller/flights.controller.ts
+++ src/flights/controller/flights.controller.ts
@@ -1,10 +1,35 @@
 import {
+    Body,
     Controller,
+    Get,
+    Param,
+    ParseIntPipe,
+    Post,
 } from '@nestjs/common';
 import { FlightsService } from '../service/flights.service';
+import { CreateFlightDto } from '../dto/create-flight.dto';
+import { FlightResponseDto } from '../dto/flight-response.dto';
 
 @Controller('flights')
 export class FlightsController {
     constructor(private readonly flightsService: FlightsService){}
-    
+
+    @Post()
+    async createFlight(
+        @Body() createFlightDto: CreateFlightDto,
+    ): Promise<FlightResponseDto> {
+        return this.flightsService.createFlight(createFlightDto);
+    }
+
+    @Get()
+    async getAllFlights(): Promise<FlightResponseDto[]> {
+        return this.flightsService.getAllFlights();
+    }
+
+    @Get(':id')
+    async getFlightById(
+        @Param('id', ParseIntPipe) id: number 
+    ): Promise<FlightResponseDto> {
+        return this.flightsService.getFlightById(id);
+    }
 }
