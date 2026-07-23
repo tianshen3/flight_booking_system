a613ee300f119f081f24759e53aa97fa0b2e9f98
Bhavesh Kumar Singh <kishornand.dib@gmail.com>
2026-07-23 19:00:43 +0530

    feat: added BookingController for managing booking operations

---
+++ src/bookings/controller/bookings.controller.ts
@@ -0,0 +1,31 @@
+import {
+    Body,
+    Controller,
+    Post,
+} from '@nestjs/common';
+import { BookingService } from '../service/bookings.service';
+import { LockSeatDto } from '../dto/lock-seat.dto';
+import { ConfirmBookingDto } from '../dto/confirm-booking.dto';
+
+@Controller('bookings')
+export class BookingController {
+    constructor(
+        private readonly bookingService: BookingService,
+    ) {}
+
+    @Post('locks')
+    lockSeat(
+        @Body() lockSeatDto: LockSeatDto,
+    ) {
+        return this.bookingService.lockSeat(lockSeatDto);
+    }
+
+
+    @Post('confirm')
+    confirmBooking(
+        @Body() dto: ConfirmBookingDto,
+    ){
+        return this.bookingService.confirmBooking(dto.bookingId);
+    }
+
+}
