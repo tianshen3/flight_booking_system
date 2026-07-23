d2f5cd4b6e8782e4a070bbe65b1a1688f92580f8
Bhavesh Kumar Singh <kishornand.dib@gmail.com>
2026-07-23 17:57:42 +0530

    feat: add @nestjs/schedule package and integrated ScheduleModule in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,12 +7,14 @@ import { UsersModule } from './users/users.module';
 import { FlightsModule } from './flights/flights.module';
 import { BookingModule } from './bookings/bookings.module';
 import { WaitlistModule } from './waitlist/waitlist.module';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    ScheduleModule.forRoot(),
     DatabaseModule,
     RedisModule,
     HealthModule,
