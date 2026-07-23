dbb30d618b50aa4f826f74fcc1d9ecc2834b663c
Bhavesh Kumar Singh <kishornand.dib@gmail.com>
2026-07-23 16:24:27 +0530

    feat: implemented lockSeat method for managing seat locking in Redis

--- src/bookings/service/seat-lock.service.ts
+++ src/bookings/service/seat-lock.service.ts
@@ -6,4 +6,44 @@ export class SeatLockService{
     constructor(
         private readonly redisService: RedisService,
     ){}
+
+    //helper method for key generation 
+    private generateSeatKey(
+        flightId: number,
+        seatId: number,
+    ): string {
+        return `seat:${flightId}:${seatId}`;
+    }
+
+    //create the lock seat method
+    async lockSeat(
+        flightId: number,
+        seatId: number,
+        userId: number,
+    ): Promise<boolean> {
+
+        //generating key using helper method
+        const key = this.generateSeatKey(flightId, seatId);
+
+        const value = {
+            userId,
+            LockedAt: new Date().toISOString(),
+        };
+
+        const serializedValue = JSON.stringify(value);
+
+        //getting redis client which is ioredis instance
+        const client = this.redisService.getClient();
+
+        //getting response from redis
+        const result = await client.set(
+            key,
+            serializedValue,
+            'EX',
+            300,
+            'NX',
+        );
+
+        return result==='OK';
+    }
 }
