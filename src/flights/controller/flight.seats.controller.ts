1d387491524a17a5a8a4680d57a5f34f00f1c7be
Bhavesh Kumar Singh <kishornand.dib@gmail.com>
2026-07-26 03:47:30 +0530

    fix: corrected flightId parameter in FlightSeatsController

--- src/flights/controller/flight.seats.controller.ts
+++ src/flights/controller/flight.seats.controller.ts
@@ -1,18 +1,22 @@
 import {
     Controller,
+    Get,
+    Param,
+    ParseIntPipe,
 } from '@nestjs/common';
 
 import { SeatsService } from '../service/seats.service';
 import { SeatResponseDto } from '../dto/seat-response.dto';
 
-@Controller('flights/:flightsId/seats')
+@Controller('flights/:flightId/seats')
 export class FlightSeatsController {
     
     constructor(private readonly seatsService: SeatsService){}
     
     //get all seats for a flight
+    @Get()
     async getSeatsByFlightId(
-        flightId: number,
+        @Param('flightId', ParseIntPipe) flightId: number,
     ): Promise<SeatResponseDto[]> {
         return this.seatsService.getSeatsByFlightId(flightId);
     }
