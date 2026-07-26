7a329b890937070388f27865c8828567e865be8a
Bhavesh Kumar Singh <kishornand.dib@gmail.com>
2026-07-26 17:41:59 +0530

    feat: enabled CORS for API to allow requests from localhost

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,11 @@ async function bootstrap() {
   );
   app.enableShutdownHooks();
 
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    credentials: true,
+  });
+
   const config = new DocumentBuilder()
   .setTitle('AeroLock API')
   .setDescription('Flight Booking System API Documentation')
